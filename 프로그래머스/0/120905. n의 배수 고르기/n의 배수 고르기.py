1612f9379104cad9182c7e6f7689e32f202dccf4
Woohyeon Roh <ykjrco@naver.com>
2025-02-08 00:09:34 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.py
@@ -0,0 +1,10 @@
+def solution(n, numlist):
+    answer = []
+    
+    for i in numlist :
+        if i % n == 0 :
+            answer.append(i)
+        else :
+            continue
+    
+    return answer

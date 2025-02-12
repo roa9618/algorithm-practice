cbc0728307ffebd7314e5537804ac7a13de58e71
Woohyeon Roh <ykjrco@naver.com>
2025-02-13 01:43:51 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.py
@@ -0,0 +1,11 @@
+def solution(num_list):
+    answer1 = ''
+    answer2 = ''
+    
+    for i in num_list :
+        if int(i) % 2 != 0 :
+            answer1 += str(i)
+        else :
+            answer2 += str(i)
+    
+    return int(answer1) + int(answer2)

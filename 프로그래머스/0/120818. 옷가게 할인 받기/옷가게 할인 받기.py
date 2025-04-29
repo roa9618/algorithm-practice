d04af7dcbe640f6576c62f5c503ad9afc4cd6f5b
Woohyeon Roh <ykjrco@naver.com>
2025-04-30 07:01:33 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 9.16 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -0,0 +1,11 @@
+import math
+
+def solution(price):
+    if price >= 500000 :
+        price -= price * 0.2
+    elif price >= 300000 :
+        price -= price * 0.1
+    elif price >= 100000 :
+        price -= price * 0.05
+    
+    return math.trunc(price)

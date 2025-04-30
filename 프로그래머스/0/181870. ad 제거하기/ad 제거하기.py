79f7e3af5984dee2476d6d05945049bec2ce05f8
Woohyeon Roh <ykjrco@naver.com>
2025-04-30 20:14:21 +0900

    [level 0] Title: ad 제거하기, Time: 0.05 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.py
@@ -0,0 +1,10 @@
+def solution(strArr):
+    answer = []
+    
+    for i in strArr :
+        if "ad" in i :
+            continue
+        else :
+            answer.append(i)
+    
+    return answer

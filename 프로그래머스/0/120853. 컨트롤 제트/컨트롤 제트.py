2a9a5e84cfdafa634d19b950586a573d09d0d88d
Woohyeon Roh <ykjrco@naver.com>
2025-05-10 17:48:30 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.04 ms, Memory: 9.23 MB -BaekjoonHub

---
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.py
@@ -0,0 +1,11 @@
+def solution(s):
+    answer = 0
+    arr = s.split()
+    
+    for i in range(len(arr)) :
+        if arr[i] == 'Z' :
+            answer -= int(arr[i - 1])
+        else :
+            answer += int(arr[i])
+    
+    return answer
